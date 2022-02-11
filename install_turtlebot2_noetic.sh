c5b4fa8f28b9856f4decf34beb56311b9d45bd3e
VeryHardBit <yanothaic8@gmaill.com>
2022-02-12 00:27:15 +0700

    yes

--- install_turtlebot2_noetic.sh
+++ install_turtlebot2_noetic.sh
@@ -12,6 +12,7 @@ sudo apt-get install ros-noetic-hector-slam
 
 echo "[Creating turtlebot2 workspace]"
 ws_name="turtlebot2_robotcitizen_ws"
+ws_name="turtlebot2_ws"
 mkdir -p $ws_name/src
 cd $ws_name
 ws_name=$(pwd)
